63fab0614ae8997cee5e2d8e7dfcc7d89cd92158
SwamWithTurtles <12e.david@gmail.com>
2014-04-04 12:10:51 +0100

    Get node application communicating with rest layer

--- royalties-ui/routes/index.js
+++ royalties-ui/routes/index.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
+var rest = require("restler");
+var _ = require('lodash');
+
+var authors;
+rest.get('http://localhost:8080/royalties-api/author').on('complete', function(response) {
+    console.log(response);
+    authors = _.pluck(response, "name").join(", ");
+});
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: authors });
 };
