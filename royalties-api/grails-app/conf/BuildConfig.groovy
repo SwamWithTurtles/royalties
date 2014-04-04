fc9278f035820726790e6c3b6bb997b80a765255
SwamWithTurtles <12e.david@gmail.com>
2014-04-04 10:55:12 +0100

    Upgrade to grails 2.3.4

--- royalties-api/grails-app/conf/BuildConfig.groovy
+++ royalties-api/grails-app/conf/BuildConfig.groovy
@@ -53,10 +53,6 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
-
-        runtime ":database-migration:1.3.2"
-
-        compile ':cache:1.0.1'
+        build ":tomcat:7.0.50"
     }
 }
